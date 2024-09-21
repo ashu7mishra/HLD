d995373927ebf8ed334b5f4f95f2618b133fa4e5
ashu7mishra <ashu7mishra@gmail.com>
2024-09-21 08:08:02 +0530

    consistant hashing code

--- 1 HLD HLD Basics and Consistent Hashing/ConsistantHashing.py
+++ 1 HLD HLD Basics and Consistent Hashing/ConsistantHashing.py
@@ -12,14 +12,34 @@ def userHash(serverId, hashKey):
         p_pow = (p_pow * p) % n
     return hashCode
 
+def assignRequest(keyname, hashkey):
+    global locationToServerMapping, serverToKeyMapping
+    if not locationToServerMapping:
+        return 1000
+    keyLocation = userHash(keyname, hashkey)
+    serverLocations = sorted(locationToServerMapping.keys())
+    for loc in serverLocations:
+        if loc >= keyLocation:
+            serverId = locationToServerMapping[loc]
+            if serverId not in serverToKeyMapping:
+                serverToKeyMapping[serverId] = []
+            serverToKeyMapping[serverId].append((keyname, hashkey))
+            return loc
+    serverId = locationToServerMapping[serverLocations[0]]
+    if serverId not in serverToKeyMapping:
+        serverToKeyMapping[serverId] = []
+    serverToKeyMapping[serverId].append((keyname, hashkey))
+    return serverLocations[0]
+
+
 def findRequestsToServer(serverLocation):
     global locationToServerMapping, serverToKeyMapping
     if not serverToKeyMapping:
         return
 
-    serverLocation = sorted(locationToServerMapping.keys())
+    serverLocations = sorted(locationToServerMapping.keys())
 
-    for loc in serverLocation:
+    for loc in serverLocations:
         if loc > serverLocation:
             serverId = locationToServerMapping[loc]
             keynames = serverToKeyMapping.get(serverId, [])
@@ -27,17 +47,32 @@ def findRequestsToServer(serverLocation):
             for keyname, hashKey in keynames:
                 assignRequest(keyname, hashKey)
             return
-    serverId = locationToServerMapping[serverLocation[0]]
+    serverId = locationToServerMapping[serverLocations[0]]
     keynames = serverToKeyMapping.get(serverId, [])
     serverToKeyMapping[serverId] = []
     for keyname, hashKey in keynames:
         assignRequest(keyname, hashKey)
+
 def addServer(serverId, hashKey):
     global locationToServerMapping, serverToKeyMapping
     firstLocation = userHash(serverId, hashKey)
     locationToServerMapping[firstLocation] = serverId
     findRequestsToServer(firstLocation)
     return len(serverToKeyMapping.get(serverId, []))
+
+def removeServer(serverId):
+    global locationToServerMapping, serverToKeyMapping
+    for loc, server in locationToServerMapping.items():
+        if server == serverId:
+            locationToServerMapping.pop(loc)
+            break
+    keynamesToReassign = serverToKeyMapping.get(serverId, [])
+    if serverId in serverToKeyMapping:
+        del serverToKeyMapping[serverId]
+    for keyname, hashkey in keynamesToReassign:
+        assignRequest(keyname, hashkey)
+    return len(keynamesToReassign)
+
 def performOperation(operation, keyOrServer, hashKey):
     global answers
 
@@ -54,7 +89,7 @@ class Solution:
     # @param C : list of integers
     # @return a list of integers
     def solve(self, A, B, C):
-        global answer, serverToKeyMapping, locationToServerMapping
+        global answers, serverToKeyMapping, locationToServerMapping
         answers = []
         serverToKeyMapping.clear()
         locationToServerMapping.clear()
@@ -62,4 +97,12 @@ class Solution:
         for i in range(len(A)):
             performOperation(A[i], B[i], C[i])
         return answers
+
+A = ["ADD", "ASSIGN", "ADD", "ASSIGN", "REMOVE", "ASSIGN"]
+B = ["INDIA", "NWFJ", "RUSSIA", "OYVL", "INDIA", "IGAX"]
+C = [7, 3, 5, 13, -1, 17 ]
+
+obj = Solution()
+out = obj.solve(A, B, C)
+print(out)
 
